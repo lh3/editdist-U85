31d9a125dbd8ed08f07b98f54fee72cd48b72ef5
Heng Li <lh3@me.com>
2022-04-09 14:17:58 -0400

    first algorithm

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <zlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ketopt.h"
+#include "kalloc.h"
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+int32_t u85_1(int32_t tl, const char *ts, int32_t ql, const char *qs);
+
+int main(int argc, char *argv[])
+{
+	gzFile fp1, fp2;
+	kseq_t *ks1, *ks2;
+	ketopt_t o = KETOPT_INIT;
+	int c, algo = 1;
+
+	while ((c = ketopt(&o, argc, argv, 1, "a:", 0)) >= 0) {
+		if (o.opt == 'a') algo = atoi(o.arg);
+	}
+	if (argc - o.ind < 2) {
+		fprintf(stderr, "Usage: test-u85 [options] <in1.fa> <in2.fa>\n");
+		fprintf(stderr, "Options:\n");
+		fprintf(stderr, "  -a INT   algorithm [%d]\n", algo);
+		return 1;
+	}
+
+	fp1 = gzopen(argv[o.ind+0], "r");
+	fp2 = gzopen(argv[o.ind+1], "r");
+	assert(fp1 && fp2);
+	ks1 = kseq_init(fp1);
+	ks2 = kseq_init(fp2);
+
+	while (kseq_read(ks1) >= 0 && kseq_read(ks2) >= 0) {
+		int s = -1;
+		if (algo == 1) s = u85_1(ks1->seq.l, ks1->seq.s, ks2->seq.l, ks2->seq.s);
+		printf("%s\t%s\t%d\n", ks1->name.s, ks2->name.s, s);
+	}
+
+	kseq_destroy(ks1);
+	kseq_destroy(ks2);
+	gzclose(fp1);
+	gzclose(fp2);
+	return 0;
+}
---
+++ u85-1.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define WF_NEG_INF (-0x40000000)
+
+int32_t u85_1(int32_t tl, const char *ts, int32_t ql, const char *qs)
+{
+	int32_t *a[2], *H, lo = 0, hi = 0, s = 0;
+	a[0] = (int32_t*)malloc((tl + ql + 5) * 2 * sizeof(int32_t));
+	a[1] = a[0] + tl + ql + 5;
+	H = a[1] + 2;
+	H[0] = -1, H[-2] = H[-1] = H[1] = H[2] = WF_NEG_INF; // pad with WF_NEG_INF
+	while (1) {
+		int32_t d, *G;
+		for (d = lo; d <= hi; ++d) {
+			int32_t k = H[d], i = d + k;
+			if (k < -1 || i < -1) continue;
+			while (k + 1 < tl && i + 1 < ql && ts[k+1] == qs[i+1])
+				++k, ++i;
+			if (k == tl - 1 && i == ql - 1) break;
+			H[d] = k;
+		}
+		if (d <= hi) break;
+		if (lo > -tl) --lo;
+		if (hi <  ql) ++hi;
+		G = a[s&1] + 2 - lo;
+		for (d = lo; d <= hi; ++d) {
+			int32_t k = H[d-1];
+			k = k > H[d]   + 1? k : H[d]   + 1;
+			k = k > H[d+1] + 1? k : H[d+1] + 1;
+			G[d] = k;
+		}
+		G[lo-2] = G[lo-1] = G[hi+1] = G[hi+2] = WF_NEG_INF;
+		H = G;
+		++s;
+	}
+	free(a[0]);
+	return s;
+}
